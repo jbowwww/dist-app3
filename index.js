c7a4fa6bf3c2fbaa413276adcaf360d4dfaa6b0f
Joely <jbowwww@gmail.com>
2019-02-14 08:03:51 +0800

    basic index.js fsIterate scan working with generator

--- index.js
+++ index.js
@@ -14,12 +14,6 @@ const fs = require('fs');
 const fsIterate = require('./fs/iterate.js').iterate;
 const mongoose = require('mongoose');	
 mongoose.Promise = Q;
-const PromisePipe = require('./promise-pipe.js');
-const pEvent = require('p-event');
-const pMap = require('p-map');
-const stream = require('stream');
-stream.finished = util.promisify(stream.finished);
-const through2Concurrent = require('through2-concurrent');
 
 	// const FileSys = require('./model/filesys');
 	const Disk = require('./model/filesys/disk.js');
@@ -29,49 +23,15 @@ const through2Concurrent = require('through2-concurrent');
 	const Audio = require('./model/audio.js');
 
 var searches = [
-	// { path: '/mnt/Stor', maxDepth: 0 },
-	// { path: '/mnt/Storage', maxDepth: 0 },
-	// { path: '/media/jk/Backup/RECOVERED_FILES/mystuff/Backup', maxDepth: 0 },
-	// { path: '/media/jk/My Passport', maxDepth: 0 },
 	{ path: '/mnt/media', maxDepth: 2 }
 	// { path: '/', maxDepth: 0, filter: dirEntry => (!['/proc', '/sys', '/lib', '/lib64', '/bin', '/boot', '/dev' ].includes(dirEntry.path)) }
 ];
 
 const app = require('./app.js');
-var promisePipeOptions = { catchErrors: app.onError };
-/* var pipelines = require('./pipelines.js');
- * var pipelines = {
- * 	debug: tap(a => { console.verbose(`\n!!\n\na.isNew=${a.isNew} a.isModified=${a.isModified} a.modifiedPaths=${a.modifiedPaths}\na = ${inspect(a)}\n\n!!\n`); }),
- * 	bulkSave: a => a.bulkSave(),
- * 	doHash: iff( a => a.file && (!a.file.hash || !a.file.hashUpdated < a.file._ts.updated),	a => a.file.doHash() ),
- * 	doAudio: iff( a => a.file && (/^.*\.(wav|mp3|mp4|au|flac)$/i).test(a.file.path),
- * 		iff( a => !a.audio,	a => a.addMetaData('audio', {}) ),
- * 		iff( a => !a.audio.isCheckedSince(a.file._ts.updatedAt), a => a.audio.loadMetadata(a.file) ) )
- * };
- */
-/* New idea - use Artefact to define promise pipes, something like:
- * Artefact.pipeFrom(fsIterate(search), )
- * or maybe
- *
- * fsIterate.pipe(FsEntry.artefactPipe({
- *	 [pipeline ops...]
- * ))
- *
- * ..yeah fuck i dunno haha
- * but see if you can clean up this syntax a bit by putting it into Artefact
- * and combine that with (optionally?) specifying extra types to include in the artefact
- * (artefact still needs an initial mongo doc (newly created or retrieved) to go and find other types associated with the artefact)
- * (how to map dependencies/ordering of type construction in artefacts? e.g. file -> audio -> sample)
- */
-
 
 app.dbConnect()
 .then(() => Disk.findOrPopulate())
-.then(async () => Q.all( _.map( searches, async search => {
-	
-	// stream.finishedpEvent(fsIterate(search).pipe(
-		// PromisePipe({ concurrency: 1 },
-
+.then(() => Q.all( _.map( searches, async search => {
 		for await (let f of fsIterate(search)) {
 			f = await FsEntry.findOrCreate(f);
 			console.log(`f.path: '${f.path}'`);
