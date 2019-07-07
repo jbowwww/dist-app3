3a00c7b7b9d3370711d02fdd6928bd8a75604559
Comment <jbowwww@gmail.com>
2019-07-07 16:09:27 +0800

    Start tidying iterate

--- fs/iterate.js
+++ fs/iterate.js
@@ -44,89 +44,57 @@ function createFsItem(path, stats) {
 // data processing fn?) IDK .. but iterate should maintain an object with total numbers (+sizes if incl stats) of FS entries under path,
 // total processed, dirs/files counts and progress (raw and % virtual) and ETA
 async function* iterate(options) {
+
 	options = _.defaults(options, {
-		path: '.',
+		path: nodePath.resolve(options.path || '.'),
 		maxDepth: 1,
-		// queueMethod: 'shift',
-		filter: undefined,
-		// removePathPrefix: undefined,
+		filter: item => true,
 		objectMode: true,
 		highWaterMark: 16,
-		handleError(err) {
-			console.warn(`iterate: ${err}`);//${err.stack||err}`);
-		}
-	});
-	
-	var path = nodePath.resolve(options.path);
-	console.verbose(`iterate('${path}', ${inspect(options, { compact: false })})`);
+		handleError(err) { console.warn(`iterate: ${err/*.stack*/}`); }
+	});	
+	console.verbose(`iterate(${inspect(options, { compact: false })})`);
   	
 	var self = {
-		
 		root: path,
-		// rootDepth: path.split(nodePath.sep).length - 1,
 		rootItem: null,
 		paths: [path],
 		errors: [],
 		promisePipe(...args) { return /*stream.finished*/(pipeline(self, new PromisePipe(...args).stream())); },
 		task: { max: 1, current: 0 }
 	};
-// _.assign(iterate.prototype, self);
-	// yield (async function* next() {
-		for (let i = 0; i < self.paths.length; i++) { //self.paths.length) {
-			var path = self.paths[i];//options.queueMethod]();
-			
-			self.task.max = self.paths.length - 1;
-			self.task.current = i;
 
-			try {
-				var stats = await nodeFs.lstat(path);
-				var item = createFsItem(path, stats);
-				if (path === self.root) {
-					self.rootItem = item;
-				}
-
-				if (!options.filter || options.filter(item)) {
-					var currentDepth = item.pathDepth; - self.rootItem.pathDepth/*self.rootDepth*/;	// +1 because below here next files are read from this dir
-					if (item.fileType === 'dir' && ((options.maxDepth === 0) || (currentDepth <= options.maxDepth + self.rootItem.pathDepth/*self.rootDepth*/))/* && (!options.filter || options.filter(item))*/) {
-						var names = await nodeFs.readdir(item.path);
-						// .then(names => {
-						// if (options.filter) names = names.filter(typeof options.filter !== 'function' ? name => name.match(options.filter): options.filter);
-						console.debug(`${names.length} entries at depth=${currentDepth} in dir:${item.path} self.paths=[${self.paths.length}] item=${inspect(item)}`);
-						_.forEach(names, name => self.paths.push(/*{ path:*/ nodePath.join(item.path, name)/*, dir: item, drive*/ /*}*/));
-
-						yield item;
-						// next();
-						// }).catch(err => nextHandleError(err));
-					} else {
-						yield item;
-						// next();
-					}
-				}
-			} catch (e) {
-				nextHandleError(e);
+	for (let i = 0; i < self.paths.length; i++) { // as long as JS will re-evaluate self.paths.length each iteration? because self.paths continues growing
+		var path = self.paths[i];
+		// self.task.max = self.paths.length - 1;
+		// self.task.current = i;
+		try {
+			var stats = await nodeFs.lstat(path);
+			var item = createFsItem(path, stats);
+			if (path === self.root) {
+				self.rootItem = item;
 			}
+			if (!options.filter || options.filter(item)) {
+				var currentDepth = item.pathDepth; - self.rootItem.pathDepth/*self.rootDepth*/;	// +1 because below here next files are read from this dir
+				if (item.fileType === 'dir' && ((options.maxDepth === 0) || (currentDepth <= options.maxDepth + self.rootItem.pathDepth/*self.rootDepth*/))/* && (!options.filter || options.filter(item))*/) {
+					var names = await nodeFs.readdir(item.path).filter(options.filter);
+					console.debug(`${names.length} entries at depth=${currentDepth} in dir:${item.path} self.paths=[${self.paths.length}] item=${inspect(item)}`);
+					_.forEach(names, name => self.paths.push(/*{ path:*/ nodePath.join(item.path, name)/*, dir: item, drive*/ /*}*/));
+
+					yield item;
+				} else {
+					yield item;
+				}
+			}
+		} catch (e) {
+			self.errors.push(err);
+			options.handleError(e);
 		}
-	// })();
-			// .catch(err => {
-			// 	console.error(err);
-			// 	return next()
-			// });
-		
-		// }
-	// })();
+	}
+
 	if (self.errors.length) {
 		console.warn(`iterate('${self.root}'): stream end: ${self.errors.length} errors: ${self.errors.join('\n\t')}`);
 	} else {
 		console.debug(`iterate('${self.root}'): stream end`);
-	}
-	
-	function nextHandleError(err) {
-		options.handleError(err);
-		self.errors.push(err);
-		console.warn(`iterate('${self.root}'): ${err.stack||err}`);
-		// process.nextTick(() =>
-		 // self.emit('error', err);
-		 // );
-		// return next();//1;
 	}
 }
