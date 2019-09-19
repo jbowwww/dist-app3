64e904bb2132af4927586afee615dfec66f10e21
Comment <jbowwww@gmail.com>
2019-09-20 01:51:57 +0800

    Remove Q from fs/hash.js

--- fs/hash.js
+++ fs/hash.js
@@ -9,11 +9,11 @@ const _ = require('lodash')
 // , bindMethods = require('./utility.js').bindMethods
 , pipeline = require('../utility.js').pipeline
 // ,	util = require('util')
-,	fs = require('fs')
+,	fs = require('fs');
 // , nodePath = require('path')
 // , EventEmitter = require('events')//emitter3');
 // ,	stream = require('stream')
-,	Q = require('q')
+// ,	Q = require('q')
 Q.longStackSupport = true;
 // ,	promisifyEmitter = require('./utility.js').promisifyEmitter
 const crypto = require('crypto');
@@ -24,7 +24,8 @@ module.exports = function hash(path, options) {
 	var options = Object.assign({}, { algorithm: 'sha256', encoding: 'hex' }, options);
 	var hashStream = crypto.createHash(options.algorithm);
 	hashStream.setEncoding(options.encoding);
-	return Q(pEvent(pipeline(fs.createReadStream(path, options), hashStream), 'data'))
+	return /*Q*/(pEvent(pipeline(
+		fs.createReadStream(path, options), hashStream), 'data'))
 	.then(hashData => hashData.toString(options.encoding));
 	// .catch(err => console.error(`Hashing error: ${err.stack||err}`));
 }
