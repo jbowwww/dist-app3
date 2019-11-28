c5d27935ec56ee1da5cd4cc57b476d3c6e1419a9
Comment <jbowwww@gmail.com>
2019-11-28 15:35:46 +0800

    This is for the version of @jbowwww/source that does streaming async, combine and EE translation all in one func

--- index-new-2.js
+++ index-new-2.js
@@ -1,21 +1,23 @@
 
 "use strict";
-const console = require('./stdio.js').Get('index', { minLevel: 'verbose' });	// debug verbose log
+// const cluster = require('cluster');
+// const debug = (...args) => require('debug')('index')(`${cluster.isWorker?'#'+cluster.worker.id+' ':''}`, ...args);
+// const console = require('./stdio.js').Get('index', { minLevel: 'verbose' });	// debug verbose log
 const inspect = require('./utility.js').makeInspect({ depth: 3, compact: true });
 const util = require('util');
-const obj = require('@jbowwww/object-util');
-const { copy: assign} = obj;
+// const obj = require('@jbowwww/object-util');
 const { EventEmitter } = require('events');
 const FsIterable = require('@jbowwww/fs-iterable');
 const pMap = require('p-map');
 const pDelay = ms => new Promise((resolve, reject) => setTimeout(resolve, ms));
-const pEvent = (em, ev) => new Promise((resolve, reject) => em.once(ev, resolve).on('error', reject));/*require('p-event');*/
+const pEvent = require('@jbowwww/event');//(em, ev) => new Promise((resolve, reject) => em.once(ev, resolve).on('error', reject));/*require('p-event');*/
 
 // const Recur = async function(fn, interval = 60000) { while (1) { await fn(); await pDelay(interval); } };
 const Limit = require('@jbowwww/limit');
 const source = require('@jbowwww/source');
-const streamAsync = require('@jbowwww/stream-async');
+// const streamAsync = require('@jbowwww/stream-async');
 const clusterProcesses = require('@jbowwww/cluster-processes');
+const debug = require('@jbowwww/debug')('index')
 
 const app = require('./app.js');
 const Disk = require('./model/filesys/disk.js');
@@ -25,8 +27,8 @@ const Dir = require('./model/filesys/dir.js');
 const Audio = require('./model/audio.js');
 
 var searches = [
-	{ path: '/etc', maxDepth: 0 },
-	{ path: '/mnt/media', maxDepth: 0 },
+	{ path: '/etc', maxDepth: 0, progress: true },
+	{ path: '/mnt/media', maxDepth: 0, progress: true },
 	// { path: '/mnt/mystuff', maxDepth: 0 }
 	// { path: '/mnt/Stor2/mystuff', maxDepth: 0 }
 	// { path: '/', maxDepth: 0, filter: dirEntry => (!['/proc', '/sys', '/lib', '/lib64', '/bin', '/boot', '/dev' ].includes(dirEntry.path)) }
@@ -42,40 +44,52 @@ var searches = [
 			async function populate () {
 				await pMap(searches, async search => {
 					try {
-						await streamAsync(
-							source(new FsIterable(obj.assign(search, { progress: true }))),
-							Limit({ concurrency: 1 }, async function (f) {
+						// for await (const f of
+						await source(new FsIterable(search)).pipe(
+							/*Limit({ concurrency: 1 },*/ async function (f) {
 								try {
-									console.verbose(`this=${inspect(this)} f=${inspect(f)}`);
+									debug(`this=${inspect(this)} f=${inspect(f)}`);
 									let a = await (await FsEntry.findOrCreate(f)).getArtefact();
-									!!a.file && //await a.file.doHash();
+									// !!a.file && //await a.file.doHash();
 									await (!!a.dir ? a.save() : a.bulkSave({ maxBatchSize: 20, batchTimeout: 2000 })); 
 									// TODO: Make an option on Artefact/model that enables calls to .save() to pass to bulkSave(), which woudl eliminate the need for this step here
 								} catch (e) {
-									console.warn(`warn for search: ${inspect(search)}: ${e.stack||e}`);
+									debug(`warn for search: ${inspect(search)}: ${e.stack||e}`);
 								}
-							}));
+							});
+						// }
 					} catch (e) {
-						console.error(`error for search: ${inspect(search)}: ${e.stack||e}`);
+						debug(`error for search: ${inspect(search)}: ${e.stack||e}`);
 					}			
 				});
+				app.logStats();
 			},
 
 			async function hash () {
-				await streamAsync(
-					File.find({ hash: { $exists: false } }).cursor(),
-					source(
-						File.watch([], { fullDocument: 'updateLookup' }), { event: 'change' }
-						async change => { 
-							console.log(`change=${inspect(change)}`);
+				try {
+					// for await (const f of
+					await source(
+						File.find({ hash: { $exists: false } }).cursor(),
+						source(File.watch([], { fullDocument: 'updateLookup' }), { event: 'change' })
+						.pipe(async function (change) { 
+							debug(`change=${inspect(change)}`);
 							return await FsEntry.hydrate(change.fullDocument);
-						}),
-					async f => {
-						console.log(`f=${inspect(f)}`); 
-						await f.doHash();
-						await f.save();
-						console.log(`fullDoc.hash.save=${inspect(f)}`); 
-					});
+						}))
+					.pipe(
+						/*Limit*/(/*{ concurrency: 1 },*/ async function(f) {
+							try {
+								debug(`this=${inspect(this)} f=${inspect(f)}`); 
+								await f.doHash();
+								await f.save();
+								debug(`fullDoc.hash.save=${inspect(f)}`);
+							} catch (e) {
+								debug(`warn for hash: f=${inspect(f)}: ${e.stack||e}`);
+							} 
+						}));
+				} catch (e) {
+					debug(`error for hash: ${e.stack||e}`);
+				}
+				app.logStats();			
 			},
 
 			// async function hashExistingFiles() {
@@ -83,36 +97,51 @@ var searches = [
 			// 		async f => {
 			// 			await f.doHash();
 			// 			await f.save();
-			// 			console.log(`fullDoc.hash.save=${inspect(f)}`);
+			// 			debug(`fullDoc.hash.save=${inspect(f)}`);
 			// 		});
 			// },
 
 			async function populateAudio() {
-				await streamAsync(
-					source(File.watch([], { fullDocument: 'updateLookup' }), { event: 'change' }), 
-					async change => {
-						const f = await FsEntry.hydrate(change.fullDocument);
-						let a = await (await FsEntry.findOrCreate(f)).getArtefact();
-						if (a.file && (/^.*\.(wav|mp3|mp4|au|flac)$/i).test(a.file.path)) {
-							console.log(`populateAudio: a=${inspect(a)}`); 
-							if (!a.audio) {
-								await a.addMetaData('audio', {});
+				try {
+					// for await (const f of 
+					await source(
+						File.find({ hash: { $exists: false } }).cursor(),					
+						source(File.watch([], { fullDocument: 'updateLookup' }), { event: 'change' })
+						.pipe(async change => { 
+							debug(`File.watch this=${inspect(this)} change=${inspect(change)}`);
+							return await FsEntry.hydrate(change.fullDocument);
+						}))
+					.pipe(
+						/*Limit*/(/*{ concurrency: 1 },*/ async function (f) {
+							try {
+								let a = await (await FsEntry.findOrCreate(f)).getArtefact();
+								if (a.file && (/^.*\.(wav|mp3|mp4|au|flac)$/i).test(a.file.path)) {
+									debug(`populateAudio: a=${inspect(a)}`); 
+									if (!a.audio) {
+										await a.addMetaData('audio', {});
+									}
+									if (a.audio.isNew || !a.audio.isCheckedSince(a.file._ts.updatedAt)) {
+										await a.audio.loadMetadata(a.file);	
+									}
+									if (a.audio && (a.audio.isNew || a.audio.isModified)) {
+										debug(`populateAudio: a2=${inspect(a)}`);
+									}
+									await a.save();	//bulkSave
+								}
+							} catch (e) {
+								debug(`warn for audio: f=${inspect(f)} ${e.stack||e}`);	
 							}
-							if (a.audio.isNew || !a.audio.isCheckedSince(a.file._ts.updatedAt)) {
-								await a.audio.loadMetadata(a.file);	
-							}
-							if (a.audio && a.audio.isNew || a.audio.isModifed) {
-								console.log(`populateAudio: a2=${inspect(a)}`);
-							}
-							await a.save();	//bulkSave
-						}
-					});
+						}));
+				} catch (e) {
+					debug(`error for audio: ${e.stack||e}`);
+				}
+				app.logStats();
 			}
 
 		);
 	
 	} catch (err) {
-		console.error(`Overall cluster error: ${err.stack||err}`);
+		debug(`Overall cluster error: ${err.stack||err}`);
 	}
 }) ();
 
